4a6f97df86c9ee3f9533f24b37742358267cad55
runtequn <rions.garden@icloud.com>
2020-05-26 11:21:36 +0900

    Add: テストケースの雛形追加

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Task, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validation' do
+    it 'is valid with all attributes' do end
+    it 'is invalid without title' do end
+    it 'is invalid without status' do end
+    it 'is invalid with a duplicate title' do end
+    it 'is valid with another title' do end
+  end
 end
+
