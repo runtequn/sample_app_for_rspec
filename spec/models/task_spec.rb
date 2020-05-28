7860184a51c12c0ff6230de5b8b80bd503c5767c
runtequn <rions.garden@icloud.com>
2020-05-28 13:32:14 +0900

    Fix: be_validマッチャを使用

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -5,21 +5,21 @@ RSpec.describe Task, type: :model do
     it 'is valid with all attributes' do
       user = User.create(email: "user@example.com", password: "password")
       task = user.tasks.new(title: "aaa", content: "aaa", status: :todo)
-      expect(task.valid?).to be(true)
+      expect(task).to be_valid
       expect(task.errors).to be_empty
     end
 
     it 'is invalid without title' do
       user = User.create(email: "user@example.com", password: "password")
       task = user.tasks.new(title: "", content: "aaa", status: :todo)
-      expect(task.valid?).to be(false)
+      expect(task).to be_invalid
       expect(task.errors[:title]).to eq ["can't be blank"]
     end
 
     it 'is invalid without status' do
       user = User.create(email: "user@example.com", password: "password")
       task = user.tasks.new(title: "aaa", content: "aaa", status: nil)
-      expect(task.valid?).to be(false)
+      expect(task).to be_invalid
       expect(task.errors[:status]).to eq ["can't be blank"]
     end
 
@@ -27,7 +27,7 @@ RSpec.describe Task, type: :model do
       user = User.create(email: "user@example.com", password: "password")
       user.tasks.new(title: "aaa", content: "aaa", status: :todo).save
       task_with_duplicated_title = user.tasks.new(title: "aaa", content: "aaa", status: :todo)
-      expect(task_with_duplicated_title.valid?).to be(false)
+      expect(task_with_duplicated_title).to be_invalid
       expect(task_with_duplicated_title.errors[:title]).to eq ["has already been taken"]
     end
 
@@ -35,7 +35,7 @@ RSpec.describe Task, type: :model do
       user = User.create(email: "user@example.com", password: "password")
       user.tasks.new(title: "aaa", content: "aaa", status: :todo).save
       task_with_another_title = user.tasks.new(title: "bbb", content: "aaa", status: :todo)
-      expect(task_with_another_title.valid?).to be(true)
+      expect(task_with_another_title).to be_valid
       expect(task_with_another_title.errors).to be_empty
     end
   end
