810d06fdac39b50be2b8779831c1a58fb2aa9e8c
runtequn <rions.garden@icloud.com>
2020-05-28 12:42:35 +0900

    Fix: 存在しないドメインである`@example.com`を使用

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -3,28 +3,28 @@ require 'rails_helper'
 RSpec.describe Task, type: :model do
   describe 'validation' do
     it 'is valid with all attributes' do
-      user = User.create(email: "user@sample.com", password: "password")
+      user = User.create(email: "user@example.com", password: "password")
       task = user.tasks.new(title: "aaa", content: "aaa", status: :todo)
       expect(task.valid?).to be(true)
       expect(task.errors).to be_empty
     end
 
     it 'is invalid without title' do
-      user = User.create(email: "user@sample.com", password: "password")
+      user = User.create(email: "user@example.com", password: "password")
       task = user.tasks.new(title: "", content: "aaa", status: :todo)
       expect(task.valid?).to be(false)
       expect(task.errors).not_to be_empty
     end
 
     it 'is invalid without status' do
-      user = User.create(email: "user@sample.com", password: "password")
+      user = User.create(email: "user@example.com", password: "password")
       task = user.tasks.new(title: "aaa", content: "aaa", status: nil)
       expect(task.valid?).to be(false)
       expect(task.errors).not_to be_empty
     end
 
     it 'is invalid with a duplicate title' do
-      user = User.create(email: "user@sample.com", password: "password")
+      user = User.create(email: "user@example.com", password: "password")
       task1 = user.tasks.new(title: "aaa", content: "aaa", status: :todo).save
       task2 = user.tasks.new(title: "aaa", content: "aaa", status: :todo)
       expect(task2.valid?).to be(false)
@@ -32,7 +32,7 @@ RSpec.describe Task, type: :model do
     end
 
     it 'is valid with another title' do
-      user = User.create(email: "user@sample.com", password: "password")
+      user = User.create(email: "user@example.com", password: "password")
       task1 = user.tasks.new(title: "aaa", content: "aaa", status: :todo).save
       task2 = user.tasks.new(title: "bbb", content: "aaa", status: :todo)
       expect(task2.valid?).to be(true)
