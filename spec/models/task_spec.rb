474d8879877e0265b48c82486848a6cc35eacc08
runtequn <rions.garden@icloud.com>
2020-05-28 13:19:11 +0900

    Fix: バリデーションエラーのメッセージ内容を確認

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -13,14 +13,14 @@ RSpec.describe Task, type: :model do
       user = User.create(email: "user@example.com", password: "password")
       task = user.tasks.new(title: "", content: "aaa", status: :todo)
       expect(task.valid?).to be(false)
-      expect(task.errors).not_to be_empty
+      expect(task.errors[:title]).to eq ["can't be blank"]
     end
 
     it 'is invalid without status' do
       user = User.create(email: "user@example.com", password: "password")
       task = user.tasks.new(title: "aaa", content: "aaa", status: nil)
       expect(task.valid?).to be(false)
-      expect(task.errors).not_to be_empty
+      expect(task.errors[:status]).to eq ["can't be blank"]
     end
 
     it 'is invalid with a duplicate title' do
@@ -28,7 +28,7 @@ RSpec.describe Task, type: :model do
       task1 = user.tasks.new(title: "aaa", content: "aaa", status: :todo).save
       task2 = user.tasks.new(title: "aaa", content: "aaa", status: :todo)
       expect(task2.valid?).to be(false)
-      expect(task2.errors).not_to be_empty
+      expect(task2.errors[:title]).to eq ["has already been taken"]
     end
 
     it 'is valid with another title' do
