959c99ce2452664c015dc73aea5bb39fc778676e
runtequn <rions.garden@icloud.com>
2020-06-10 18:43:12 +0900

    Add: テストケースの雛形を作成

--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -1,9 +1,50 @@
 require 'rails_helper'
 
-RSpec.describe "Tasks", type: :system do
-  before do
-    driven_by(:rack_test)
+RSpec.describe 'Tasks', type: :system do
+  describe 'ログイン前' do
+    describe 'ページ遷移確認' do
+      context 'タスクの新規登録ページにアクセス' do
+        it '新規登録ページへのアクセスが失敗する' 
+      end
+      context 'タスクの編集ページにアクセス' do
+        it '編集ページへのアクセスが失敗する'
+      end
+      context 'タスクの詳細ページにアクセス' do
+        it 'タスクの詳細情報が表示される'
+      end
+      context 'タスクの一覧ページにアクセス' do
+        it 'すべてのユーザーのタスク情報が表示される' 
+      end
+    end
   end
 
-  pending "add some scenarios (or delete) #{__FILE__}"
+  describe 'ログイン後' do
+    describe 'タスク新規登録' do
+      context 'フォームの入力値が正常' do
+        it 'タスクの新規作成が成功する'
+      end
+      context 'タイトルが未入力' do
+        it 'タスクの新規作成が失敗する'
+      end
+      context '登録済のタイトルを入力' do
+        it 'タスクの新規作成が失敗する'
+      end
+    end
+
+    describe 'タスク編集' do
+      context 'フォームの入力値が正常' do
+        it 'タスクの編集が成功する' do
+       end
+      context 'タイトルが未入力' do
+        it 'タスクの編集が失敗する'
+      end
+      context '登録済のタイトルを入力' do
+        it 'タスクの編集が失敗する' 
+      end
+    end
+
+    describe 'タスク削除' do
+      it 'タスクの削除が成功する'
+    end
+  end
 end
